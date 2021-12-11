7044827bca325bc8aabb9972ccfa002019874a35
Kole Davis <koledavis@MacBook-Pro.local>
2021-12-11 11:22:43 -0700

    Adding the logic for converting infix into postfix

--- cs_2420/final_review.py
+++ cs_2420/final_review.py
@@ -35,3 +35,36 @@ class Stack():
 
     def clear(self):
         self.lyst = []
+
+
+#create function that take the infix expression and change it to postfix
+def in2post(expression):
+    stack_list = Stack()
+    OPERATORS = set(['+', '-', '*', '/', '(', ')', '^'])
+    PRIORITY = {'+':1, '-':1, '*':2, '/':2, '^':3}
+    NUMBERS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    """
+    :param expression:
+    :return:
+    """
+    if type(expression) is not str:
+        raise ValueError
+    expression = expression.replace(" ", "")
+    output = ''
+    for char in expression:
+        if char not in OPERATORS:
+            output += char
+        elif char == '(':
+            stack_list.push('(')
+        elif char == ')':
+            while stack_list.size() > 0 and stack_list.top() != '(':
+                output += stack_list.pop()
+            stack_list.pop()
+        else:
+            while stack_list.size() > 0 and stack_list.top() != '('\
+                    and PRIORITY[char] <= PRIORITY[stack_list.top()]:
+                output += stack_list.pop()
+            stack_list.push(char)
+    while stack_list.size() > 0:
+        output += stack_list.pop()
+    return output
